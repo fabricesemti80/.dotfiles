38ef636388dc015fac0d15430deb8bd9e315f3c3
Fabrice Semti <fabrice@fabricesemti.com>
2023-12-13 12:43:30 +0000

    refactor: 🚀 renaming stuff

--- system/configuration.nix
+++ system/configuration.nix
@@ -18,7 +18,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos-mk3-vm"; # Define your hostname.
+  networking.hostName = "nixos-vm"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
