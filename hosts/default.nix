bcaef973fb3b1b7581ba241136405a050df99af0
fabrice <fabrice@fabricesemti.com>
2023-12-16 15:26:52 +0000

    feat: 🎨 enable passwordless sudo for my user
    https://discourse.nixos.org/t/dont-prompt-a-user-for-the-sudo-password/9163/2

--- hosts/default.nix
+++ hosts/default.nix
@@ -4,4 +4,15 @@
   imports = [ # Include the results of the hardware scan.
     ./fonts.nix
   ];
+
+  #? Passwordless sudo
+  security.sudo.extraRules = [{
+    users = [ "fabrice" ];
+    commands = [{
+      command = "ALL";
+      options =
+        [ "NOPASSWD" ]; # "SETENV" # Adding the following could be a good idea
+    }];
+  }];
+
 }
