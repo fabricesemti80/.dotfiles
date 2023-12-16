2299a8fcdd231d7ef8098d6252145c04df79249a
fabrice <fabrice@fabricesemti.com>
2023-12-16 16:47:03 +0000

    refactor: ♻️ refactor flake sources

--- flake.nix
+++ flake.nix
@@ -4,35 +4,62 @@
   ##? References Used by Flake
   inputs = {
 
-    ##? Unstable Nix Packages (Default)
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url =
+      "github:nixos/nixpkgs/release-23.11"; # Unstable Nix Packages (Default)
 
-    ##? Stable Nix Packages
-    nixpkgs-stable.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs-unstable.url =
+      "github:nixos/nixpkgs/nixos-unstable"; # Stable Nix Packages
 
-    # ##? NUR Community Packages
-    # ##? Requires "nur.nixosModules.nur" to be added to the host modules
-    # nur = { url = "github:nix-community/NUR"; };
+    # nixpkgs-stable.url = "github:nixos/nixpkgs/release-23.05"; # Stable Nix Packages
 
-    ##? Fixes OpenGL With Other Distros.
+    nur = { # NUR Community Packages
+      url =
+        "github:nix-community/NUR"; # Requires "nur.nixosModules.nur" to be added to the host modules
+    };
+
     nixgl = {
       url = "github:guibou/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs"; # ? Fixes OpenGL With Other Distros.
+    };
+
+    nixvim = { # Neovim
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+
+    # emacs-overlay = { # Emacs Overlays #TODO: activate if used
+    #   url = "github:nix-community/emacs-overlay"; 
+    #   flake = false;
+    # };
+
+    # doom-emacs = { # Nix-Community Doom Emacs #TODO: activate if used
+    #   url = "github:nix-community/nix-doom-emacs";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    #   inputs.emacs-overlay.follows = "emacs-overlay";
+    # };
+
+    # hyprland = { # Official Hyprland Flake #TODO: activate if used
+    #   url =
+    #     "github:hyprwm/Hyprland"; # Requires "hyprland.nixosModules.default" to be added the host modules
+    #   inputs.nixpkgs.follows = "nixpkgs-unstable";
+    # };
+
+    # plasma-manager = { # KDE Plasma User Settings Generator #TODO: activate if used
+    #   url = "github:pjones/plasma-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    #   inputs.home-manager.follows = "nixpkgs";
+    # };
+
+    home-manager = { # User Environment Manager
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    ##? User Environment Manager
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    sops-nix.url =
+      "github:Mic92/sops-nix"; # SOPS Secret managment - "https://github.com/Mic92/sops-nix"
 
-    ##? SOPS Secret managment
-    ##? ref:  https://github.com/Mic92/sops-nix
-    sops-nix.url = "github:Mic92/sops-nix";
-
-    ##? VSCode Server support
-    ##? ref: https://github.com/nix-community/nixos-vscode-server
-    vscode-server.url = "github:nix-community/nixos-vscode-server";
+    vscode-server.url =
+      "github:nix-community/nixos-vscode-server"; # VSCode Server support - "https://github.com/nix-community/nixos-vscode-server"
 
   };
 
@@ -45,6 +72,14 @@
         config = { allowUnfree = true; };
       };
       lib = nixpkgs.lib;
+
+      vars = { # Variables Used In Flake
+        user = "fabrice";
+        # location = "$HOME/.setup"; #TODO: Change when final
+        terminal = "kitty";
+        editor = "nvim";
+      };
+
     in {
 
       homeManagerConfigurations = {
