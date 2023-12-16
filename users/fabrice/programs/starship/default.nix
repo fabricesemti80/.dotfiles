50b531dbf19f27febc0ec819c999c46eaf7c99db
fabrice <fabrice@fabricesemti.com>
2023-12-16 20:24:43 +0000

    try generate starship config

--- users/fabrice/programs/starship/default.nix
+++ users/fabrice/programs/starship/default.nix
@@ -5,4 +5,19 @@ let
     symbol = icon;
     format = "[$symbol ](${color})";
   };
-in { programs.starship = { enable = true; }; }
+in {
+  programs.starship = { enable = true; };
+  home.file = {
+    ".config/starship.toml".source = ./starship.toml;
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+}
