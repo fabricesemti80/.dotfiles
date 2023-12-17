257dce21f0e2596cb37d451f50b1a9c4acd836fb
Fabrice Semti <emilfabrice@gmail.com>
2023-12-17 18:46:15 +0000

    commit this too

--- users/fabrice/programs/git/default.nix
+++ users/fabrice/programs/git/default.nix
@@ -46,6 +46,7 @@ in {
         autocrlf = "input";
       };
 
+      # ? ref: "https://jeppesen.io/git-commit-sign-nix-home-manager-ssh/"
       commit.gpgsign = true;
       gpg.format = "ssh";
       gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
