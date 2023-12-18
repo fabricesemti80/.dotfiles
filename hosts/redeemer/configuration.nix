2998b51a1d6aa21094ae1133d42824a13cb61a4c
Fabrice Semti <emilfabrice@gmail.com>
2023-12-18 22:01:45 +0000

    refactor: ♻️ separate bootloader

--- hosts/redeemer/configuration.nix
+++ hosts/redeemer/configuration.nix
@@ -7,11 +7,6 @@
 {
   imports = [ ];
 
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
-
   networking.hostName = "redeemer"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
--- hosts/redeemer/default.nix
+++ hosts/redeemer/default.nix
@@ -4,12 +4,11 @@
   imports = [
 
     ../common/global
-
     ../common/users/fabrice
-
     ../common/optional/docker.nix
 
     ./configuration.nix
+    ./bootloader.nix
     ./hardware-configuration.nix
   ];
 }
